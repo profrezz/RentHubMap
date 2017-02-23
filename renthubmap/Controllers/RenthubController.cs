53be72c2c4b01deb92166c918722fabffc1aec30
nopparut chinnabunchonesakal <nopparut chinnabunchonesakal>
2017-02-24 00:23:20 +0700

    local first commit

---
+++ renthubmap/Controllers/RenthubController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace renthubmap.Controllers
+{
+    public class RenthubController : Controller
+    {
+        // GET: Renthub
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
